73d29d063864bc0681010819ea4e811e3893d762
Eminokanju Oghenekparobor <66335632+oghenekparobor@users.noreply.github.com>
2022-09-28 23:17:38 +0100

    Learnt moules, path

---
+++ week-1/day-2/restaurant/src/lib.rs
@@ -0,0 +1,32 @@
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn it_works() {
+//         let result = 2 + 2;
+//         assert_eq!(result, 4);
+//     }
+// }
+
+mod front_of_house {
+    mod hosting {
+        fn add_to_waitlist() {}
+
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
+    }
+}
+
+pub fn eat_at_restaurant() {
+    // absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // relative path
+    front_of_house::hosting::add_to_waitlist();
+}
---
+++ week-1/day-2/restaurant/target/debug/deps/restaurant-a5997f4feb1d119f.d
@@ -0,0 +1,7 @@
+/Users/oeminokanju/30-days-of-rust/week-1/day-2/restaurant/target/debug/deps/restaurant-a5997f4feb1d119f.rmeta: src/lib.rs
+
+/Users/oeminokanju/30-days-of-rust/week-1/day-2/restaurant/target/debug/deps/librestaurant-a5997f4feb1d119f.rlib: src/lib.rs
+
+/Users/oeminokanju/30-days-of-rust/week-1/day-2/restaurant/target/debug/deps/restaurant-a5997f4feb1d119f.d: src/lib.rs
+
+src/lib.rs:
