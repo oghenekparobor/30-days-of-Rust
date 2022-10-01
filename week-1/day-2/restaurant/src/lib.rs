cd96e31ee8ac3f9033b3e839705dc1fabe14cd27
Eminokanju Oghenekparobor <66335632+oghenekparobor@users.noreply.github.com>
2022-10-01 07:30:39 +0100

    day-3: use keyword

--- week-1/day-2/restaurant/src/lib.rs
+++ week-1/day-2/restaurant/src/lib.rs
@@ -49,19 +49,26 @@ mod back_of_house {
     }
 }
 
+// pub fn eat_at_restaurant() {
+//     // // absolute path
+//     hosting::add_to_waitlist();
+
+//     // // relative path
+//     // front_of_house::hosting::add_to_waitlist();
+//     let mut meal = back_of_house::Breakfast::summer("Rye");
+//     meal.toast = String::from("Wheat");
+
+//     println!("I'd like {} toast please", meal.toast);
+
+//     let order1 = back_of_house::Appetizer::Soup;
+//     let order2 = back_of_house::Appetizer::Salad;
+// }
+// use super::hosting;
+use crate::front_of_house::hosting::add_to_waitlist;
+
+// eat at restaurant is a different scope from the front of house
 pub fn eat_at_restaurant() {
-    // // absolute path
-    // crate::front_of_house::hosting::add_to_waitlist();
-
-    // // relative path
-    // front_of_house::hosting::add_to_waitlist();
-    let mut meal = back_of_house::Breakfast::summer("Rye");
-    meal.toast = String::from("Wheat");
-
-    println!("I'd like {} toast please", meal.toast);
-
-    let order1 = back_of_house::Appetizer::Soup;
-    let order2 = back_of_house::Appetizer::Salad;
+    add_to_waitlist();
 }
 
 fn deliver_order() {}
